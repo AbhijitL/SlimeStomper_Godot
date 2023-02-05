7c047f576ad8c6493e68b5e7f758c2cc0c630c22
AbhijitL <labhijitsingh13@gmail.com>
2023-02-05 22:19:40 +0530

    Removed groud check from player

--- Source/Entities/Player/Player.gd
+++ Source/Entities/Player/Player.gd
@@ -5,7 +5,6 @@ export var move_radius: float= 4;
 
 onready var pivot = $Pivot
 onready var collider : CollisionShape = $CollisionShape;
-onready var ground_check : RayCast = $GroundCheck;
 onready var _state_machine: StateMachine = $StateMachine;
 onready var tween : Tween = $Tween;
 onready var slime_skin : = $Pivot/Slime;
@@ -15,8 +14,6 @@ var is_active : bool = true setget set_is_active;
 
 func _ready():
 	yield(owner,"ready");
-	if ground_check:
-		ground_check.set_as_toplevel(true);
 
 func set_is_active(value:bool)->void:
 	is_active = value;
