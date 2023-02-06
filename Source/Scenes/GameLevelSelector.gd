fb580212babda3ddfe3d037849feaafff79a9e6d
AbhijitL <labhijitsingh13@gmail.com>
2023-02-06 21:11:26 +0530

    make level selector ui layer -1

---
+++ Source/Scenes/GameLevelSelector.gd
@@ -0,0 +1,12 @@
+extends Spatial
+
+onready var ui = $UI;
+
+func _ready():
+    # Events.connect("transition_to",self,"_On_transition_to");
+    pass;
+
+
+# func _On_transition_to()->void:
+#     print("called");
+#     ui.visible = not ui.visible;
