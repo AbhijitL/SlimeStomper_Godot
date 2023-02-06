74ab4072bd89fa08e524d94284a489285e02e765
AbhijitL <labhijitsingh13@gmail.com>
2023-02-06 17:31:20 +0530

    created a timer and start when a enemy is killed

--- Source/UI/GameStatsUI.gd
+++ Source/UI/GameStatsUI.gd
@@ -2,6 +2,11 @@ extends CanvasLayer
 
 onready var player_health_label : Label = $Control/MarginContainer/HBoxContainer/VBoxContainer2/VBoxContainer/HBoxContainer/HBoxContainer2/PlayerHealthLabel;
 onready var point_label : Label = $Control/MarginContainer/HBoxContainer/VBoxContainer2/VBoxContainer/HBoxContainer/HBoxContainer3/PointLabel;
+onready var time_label : Label = $Control/MarginContainer/HBoxContainer/VBoxContainer3/HBoxContainer/TimeLabel;
+
+var time : float = 0.0;
+
+var time_started : bool = false;
 
 func _input(event):
 	if event.is_action_pressed("menu_open"):
@@ -10,9 +15,27 @@ func _input(event):
 func _ready():
 	Events.connect("player_health_change",self,"_On_player_health_change");
 	Events.connect("enemy_kill_change",self,"_On_enemy_kill_change");
+	Events.connect("level_completed",self,"_On_level_completed");
 
 func _On_player_health_change(health,previous_health)->void:
 	player_health_label.text = str(health) + " hp";
 
 func _On_enemy_kill_change(point,previous_point)->void:
 	point_label.text = str(point)+" $";
+	if point == 1:
+		time_started = true;
+
+func _On_level_completed()->void:
+	set_time_to_stop();
+
+func set_time_to_stop()->void:
+	time_started = false;
+
+func _process(delta):
+	if time_started:
+		time += delta;
+		time = round_to_dec(time,2);
+		time_label.text = "Time: " + str(time)+"s";
+
+func round_to_dec(num, digit):
+    return round(num * pow(10.0, digit)) / pow(10.0, digit)
