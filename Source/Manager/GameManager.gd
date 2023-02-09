92e936459ff51f029ea70247a3c06e3da1540ccc
AbhijitL <labhijitsingh13@gmail.com>
2023-02-09 21:55:38 +0530

    add save manger instance to it.

--- Source/Manager/GameManager.gd
+++ Source/Manager/GameManager.gd
@@ -2,14 +2,20 @@ extends Node
 
 var data : Dictionary = {};
 
+var save_manager : SaveManager;
+
+func _ready():
+    save_manager = SaveManager.new();
+    data = load_data();
 
 func save_data(level : int, best_time : float, completed: bool)->void:
     var new_data : Dictionary = {level:[best_time,completed]};
     data.merge(new_data,true);
     print(data);
+    save_manager.save_data(data);
 
 func load_data() -> Dictionary:
-    return {};
+    return save_manager.load_data();
 
 func get_best_time(level:int)-> float:
     if not data.has(level):
