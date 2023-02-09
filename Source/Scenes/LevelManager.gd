6405766d63af333444de6865a4bf20558d9760b2
AbhijitL <labhijitsingh13@gmail.com>
2023-02-09 20:30:29 +0530

    get_level_name and call GM save_data;

--- Source/Scenes/LevelManager.gd
+++ Source/Scenes/LevelManager.gd
@@ -1,16 +1,21 @@
 extends Spatial
 
+var level : int = 0;
 
 onready var enemies = $Enemies;
 
 var no_of_enemy : int = 0;
-
 var point : int = 0;
 
 func _ready():
 	no_of_enemy = enemies.get_child_count();
 	Events.connect("enemy_kill_change",self,"_On_Enemy_kill_change");
+	get_level_name();
+	
 
+func get_level_name()->void:
+	var level_name = self.name;
+	level = int(level_name.left(level_name.length()-1));
 
 func _On_Enemy_kill_change(value,previous_value)->void:
 	print("enemy value chaned"+str(1));
@@ -18,3 +23,4 @@ func _On_Enemy_kill_change(value,previous_value)->void:
 	point += value;
 	if point == no_of_enemy:
 		Events.emit_signal("level_completed");
+		GameManager.save_data(level, 10.0, true);
