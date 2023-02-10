3a3b9f4777f28ef37781e4848fca724bbfb1cfda
AbhijitL <labhijitsingh13@gmail.com>
2023-02-10 17:54:06 +0530

    added a label to show time elapsed to complete lvl

--- Source/Entities/Environment/TransitionDoor.gd
+++ Source/Entities/Environment/TransitionDoor.gd
@@ -2,6 +2,8 @@ extends Spatial
 
 export var change_to_scene : String = "";
 
+onready var time_label : Label3D = $Label3D;
+
 func _on_Area_body_entered(body)->void:
     if body is Player:
         Events.emit_signal("transition_to",change_to_scene);
