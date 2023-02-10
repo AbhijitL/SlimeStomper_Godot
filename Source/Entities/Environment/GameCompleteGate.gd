a1628e9e75bf9e0651294d7f49ff32d378d3d5bf
AbhijitL <labhijitsingh13@gmail.com>
2023-02-10 17:20:49 +0530

    unlocked the lock if called

--- Source/Entities/Environment/GameCompleteGate.gd
+++ Source/Entities/Environment/GameCompleteGate.gd
@@ -6,12 +6,15 @@ export var color_no_safe : Color;
 onready var collision : CollisionShape = $GateMesh/StaticBody/CollisionShape;
 onready var mesh : MeshInstance = $GateMesh/Gate;
 
-
 func _ready():
     Events.connect("level_completed",self,"_On_level_completed");
     mesh.get_surface_material(1).set_shader_param("color",color_no_safe);
 
 func _On_level_completed():
+    collision.set_deferred("disabled", true);
+    mesh.get_surface_material(1).set_shader_param("color",color_safe);
+
+func unlocked():
     collision.set_deferred("disabled", true);
     mesh.get_surface_material(1).set_shader_param("color",color_safe);
 
